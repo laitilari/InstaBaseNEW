368de09b238266d3d93d7ce1768143d17dfbefe0
Joel Enwald <joel.enwald@yahoo.com>
2016-12-02 14:37:57 +0200

    started writing mockMvc tests

--- InstaBaseNEW/src/test/java/wad/InstaBaseNEWApplicationTest.java
+++ InstaBaseNEW/src/test/java/wad/InstaBaseNEWApplicationTest.java
@@ -1,3 +1,5 @@
+package wad;
+
 
 
 import org.junit.After;
@@ -6,28 +8,44 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import wad.domain.Account;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class InstaBaseNEWApplicationTest {
+    
+    @Autowired
+    private WebApplicationContext webAppContext;
+    
+    private MockMvc mockMvc;
     
     public InstaBaseNEWApplicationTest() {
     }
     
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
     @Before
     public void setUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webAppContext).build();
     }
     
-    @After
-    public void tearDown() {
+    @Test
+    public void RegisteringWorks() throws Exception {
+        mockMvc.perform(get("/register"))
+                .andExpect(status().isOk());
+        Account testAccount = new Account();
+        testAccount.setUsername("testUsername");
+        testAccount.setPassword("testPassword");
+        mockMvc.perform(post("/register/createuser?username=Joel&password=JoelE"));
     }
-
+    
     // TODO add test methods here.
     // The methods must be annotated with annotation @Test. For example:
     //
