9d4030cd5a95ca9c4f8697795150ed3440b5309d
Laitinen Ilari I M <ilarilai@da2-cs-z1-02.helsinki.fi>
2016-12-12 14:21:39 +0200

    js

--- src/main/resources/public/javascript/javascript.js
+++ src/main/resources/public/javascript/javascript.js
@@ -2,3 +2,8 @@ function sayHello() {
     alert("hello there");
 }
 
+function aseta() {
+	var arvo = document.getElementById("tekstikentta").value;
+	document.getElementById("kommentti").innerHTML = arvo;
+}
+
